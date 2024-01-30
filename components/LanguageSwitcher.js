4db8ccae2e2cc1b9caffc4a0c9a1ae14b4e448d7
yogesh109202 <128414694+yogesh109202@users.noreply.github.com>
2024-01-30 22:36:19 +0530

    Update LanguageSwitcher.js

--- components/LanguageSwitcher.js
+++ components/LanguageSwitcher.js
@@ -1,11 +1,11 @@
 "use client"
-import {  } from '../contexts/LanguageContext';
+import { useLanguage } from '../contexts/LanguageContext';
 
 const LanguageSwitcher = () => {
-    
+    const { language, setLanguage } = useLanguage();
 
     return (
-        <select value={language} >
+        <select value={language} onChange={(e) => setLanguage(e.target.value)}>
             <option value="en">English</option>
             <option value="fr">French</option>
             <option value="es">Spanish</option>
